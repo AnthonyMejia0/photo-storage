1acfa97a2da4a21c2a8e6d9c4a5670696a48fee2
AnthonyMejia0 <mejia.anthony96@gmail.com>
2023-01-30 03:49:33 -0800

    require login

--- src/components/UploadForm.jsx
+++ src/components/UploadForm.jsx
@@ -1,19 +1,25 @@
 import { PlusCircle } from "phosphor-react";
 import { useState } from "react";
+import { useRecoilValue } from "recoil";
+import { userState } from "../atoms/userAtom";
 import ProgressBar from "./ProgressBar";
 
 function UploadForm() {
   const [file, setFile] = useState(null);
   const [error, setError] = useState(null);
+  const user = useRecoilValue(userState);
 
   const types = ["image/png", "image/jpeg"];
 
   const handleChange = (e) => {
     let selectedFile = e.target.files[0];
 
-    if (selectedFile && types.includes(selectedFile.type)) {
+    if (selectedFile && types.includes(selectedFile.type) && user) {
       setFile(selectedFile);
       setError(null);
+    } else if (!user) {
+      setFile(null);
+      setError("You must be logged in to upload an image.");
     } else {
       setFile(null);
       setError("Please select an image file (png or jpeg).");
@@ -21,13 +27,15 @@ function UploadForm() {
   };
 
   return (
-    <div className="w-[75vw] lg:w-[60vw] mx-auto">
+    <div className="relative w-[75vw] lg:w-[60vw] mx-auto">
       <form className="w-10 mx-auto">
         <label>
           <input type="file" onChange={handleChange} hidden />
           <PlusCircle className="mx-auto mt-5 cursor-pointer text-[40px] text-blue-400 hover:opacity-80 hover:scale-105" />
         </label>
-        <div className="w-screen">{error && <div>{error}</div>}</div>
+        <div className="absolute left-[-12.5vw] lg:left-[-20vw] w-screen">
+          {error && <div className="w-max mx-auto">{error}</div>}
+        </div>
       </form>
       {file && <ProgressBar file={file} setFile={setFile} />}
     </div>
