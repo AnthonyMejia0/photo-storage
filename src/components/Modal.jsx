04708bc52fbf6b90344731a803146080bb706104
AnthonyMejia0 <mejia.anthony96@gmail.com>
2023-01-30 02:08:47 -0800

    added download and delete buttons

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,9 @@
+import { deleteDoc, doc } from "firebase/firestore";
+import { deleteObject, getBlob, ref } from "firebase/storage";
 import { motion } from "framer-motion";
+import { DownloadSimple, Trash } from "phosphor-react";
+import { Tooltip } from "react-tooltip";
+import { firestore, storage } from "../firebase/config";
 
 function Modal({ selectedImage, setSelectedImage }) {
   const handleClick = (e) => {
@@ -7,21 +12,65 @@ function Modal({ selectedImage, setSelectedImage }) {
     }
   };
 
+  const handleDownload = async () => {
+    const storageRef = ref(storage, selectedImage.name);
+    const blob = await getBlob(storageRef);
+
+    let a = document.createElement("a");
+    a.href = window.URL.createObjectURL(blob);
+    a.download = selectedImage.name;
+    a.click();
+  };
+
+  const handleDelete = async () => {
+    await deleteDoc(doc(firestore, "images", selectedImage.id));
+    await deleteObject(ref(storage, selectedImage.name));
+
+    setSelectedImage(null);
+  };
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       onClick={handleClick}
-      className="backdrop fixed top-0 left-0 w-full h-full bg-[#00000080] backdrop-blur-[1px]"
+      className="backdrop flex flex-col -space-y-36 md:-space-y-48 xl:-space-y-10 items-center fixed top-0 left-0 w-full h-full bg-[#00000080] backdrop-blur-[1px]"
     >
       <motion.img
         initial={{ y: "-100vh" }}
         animate={{ y: 0 }}
         transition={{ delay: 0.45 }}
-        className="block max-w-[80%] lg:min-w-[60%] max-h-[80%] my-60 lg:my-[60px] mx-auto shadow-lg rounded-sm border-[3px] border-solid border-white"
+        className="block max-w-[80%] max-h-[55%] xl:max-h-[70%] my-40 md:my-52 xl:my-[60px] mx-auto shadow-lg rounded-sm border-[3px] border-solid border-white"
         src={selectedImage.url}
         alt={selectedImage.name}
       />
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1.2 }}
+        className="space-x-10"
+      >
+        <button
+          onClick={handleDownload}
+          id="download-btn"
+          data-tooltip-content="Download"
+          className="bg-blue-400 p-1 rounded hover:scale-105"
+        >
+          <Tooltip anchorId="download-btn" />
+          <DownloadSimple color="black" size={32} />
+        </button>
+
+        <button
+          onClick={handleDelete}
+          id="delete-btn"
+          data-tooltip-content="Delete"
+          className="bg-red-500 p-1 rounded hover:scale-105"
+        >
+          <Tooltip anchorId="delete-btn" />
+          <Trash size={32} />
+        </button>
+      </motion.div>
     </motion.div>
   );
 }
