26020790a3078ed06024eaaf8b459e0e4c7ea857
AnthonyMejia0 <mejia.anthony96@gmail.com>
2023-01-30 03:48:41 -0800

    added recoil root

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "react-tooltip/dist/react-tooltip.css";
+import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>
+);
