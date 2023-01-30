f283dd6d5cf96fcd53071a1d54fd5276448519ac
AnthonyMejia0 <mejia.anthony96@gmail.com>
2023-01-30 03:49:01 -0800

    added menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,69 @@
+import { signOut } from "@firebase/auth";
 import { List } from "phosphor-react";
+import { Link } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { userState } from "../atoms/userAtom";
+import { auth } from "../firebase/config";
 
 function Header() {
+  const user = useRecoilValue(userState);
+  const userCopy = JSON.parse(JSON.stringify(user));
+
+  const handleAuth = async () => {
+    await signOut(auth);
+  };
+
+  const toggleMenu = (e) => {
+    let menu = document.getElementById("menu");
+
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+  };
+
   return (
     <div className="w-screen">
-      <div className="flex items-center justify-between w-[75vw] lg:w-[60vw] mx-auto py-5">
+      <div className="relative flex items-center justify-between w-[75vw] lg:w-[60vw] mx-auto py-5">
         <div className="font-bold text-2xl text-blue-400">Image Locker</div>
-        <List
-          className="cursor-pointer text-gray-700 hover:scale-105"
-          size={32}
-        />
+        <button
+          onClick={toggleMenu}
+          className="md:hidden text-gray-700 hover:scale-105"
+        >
+          <List size={32} />
+        </button>
+
+        <div className="hidden md:flex items-center justify-between space-x-4 h-full">
+          <Link to="/signup">
+            <button className="border-[1px] border-gray-500 text-white font-bold bg-gray-500 px-2 py-1 rounded hover:bg-white hover:text-gray-700">
+              Sign-up
+            </button>
+          </Link>
+
+          <Link onClick={handleAuth} to={!user && "/login"}>
+            <button className="border-[1px] border-blue-400 text-blue-400 font-bold px-2 py-1 rounded hover:bg-blue-400 hover:text-white">
+              {userCopy ? "Logout" : "Login"}
+            </button>
+          </Link>
+        </div>
+
+        <div
+          id="menu"
+          className="absolute space-x-5 text-white shadow-xl md:hidden right-0 top-[80%] bg-gray-500 p-3 rounded"
+        >
+          <Link to={!user && "/login"}>
+            <button className="hover:underline underline-offset-2">
+              {userCopy ? "Logout" : "Login"}
+            </button>
+          </Link>
+
+          <Link to={!user && "/signup"}>
+            <button className="hover:underline underline-offset-2">
+              Sign-up
+            </button>
+          </Link>
+        </div>
       </div>
 
       <h1 className="mx-auto max-w-fit text-[2.5rem] md:text-[2.75rem] mt-10 font-bold text-gray-700">
