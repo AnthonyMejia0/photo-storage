45eb9ffbfc1d2eed19390f704530250c7ce76f0f
AnthonyMejia0 <mejia.anthony96@gmail.com>
2023-01-31 01:05:35 -0800

    updated reference locations

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,17 +1,23 @@
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
+import { userState } from "../atoms/userAtom";
 import { storage, firestore } from "../firebase/config";
 
 export const useStorage = (file) => {
+  const user = useRecoilValue(userState);
   const [progress, setProgress] = useState(0);
   const [error, setError] = useState(null);
   const [url, setUrl] = useState(null);
 
   useEffect(() => {
-    const storageRef = ref(storage, file.name);
+    const storageRef = ref(
+      storage,
+      "users/" + user.uid + "/images/" + file.name
+    );
     const uploadTask = uploadBytesResumable(storageRef, file);
-    const collectionRef = collection(firestore, "images");
+    const collectionRef = collection(firestore, "users", user.uid, "images");
 
     uploadTask.on(
       "state_changed",
@@ -34,7 +40,7 @@ export const useStorage = (file) => {
         setUrl(url);
       }
     );
-  }, [file]);
+  }, [file, user]);
 
   return { progress, error, url };
 };
