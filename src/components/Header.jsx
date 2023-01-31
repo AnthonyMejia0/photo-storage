8c74fd0dccf628eba513f97382f24bad2024e271
AnthonyMejia0 <mejia.anthony96@gmail.com>
2023-01-31 01:04:31 -0800

    added account access

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { signOut } from "@firebase/auth";
-import { List } from "phosphor-react";
+import { Image, List } from "phosphor-react";
 import { Link } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { userState } from "../atoms/userAtom";
@@ -7,10 +7,11 @@ import { auth } from "../firebase/config";
 
 function Header() {
   const user = useRecoilValue(userState);
-  const userCopy = JSON.parse(JSON.stringify(user));
 
   const handleAuth = async () => {
-    await signOut(auth);
+    if (user) {
+      await signOut(auth);
+    }
   };
 
   const toggleMenu = (e) => {
@@ -26,7 +27,10 @@ function Header() {
   return (
     <div className="w-screen">
       <div className="relative flex items-center justify-between w-[75vw] lg:w-[60vw] mx-auto py-5">
-        <div className="font-bold text-2xl text-blue-400">Image Locker</div>
+        <div className="flex space-x-2">
+          <div className="font-bold text-2xl text-blue-400">Image Locker</div>
+          <Image color="#60A5FA" size={32} />
+        </div>
         <button
           onClick={toggleMenu}
           className="md:hidden text-gray-700 hover:scale-105"
@@ -36,30 +40,40 @@ function Header() {
 
         <div className="hidden md:flex items-center justify-between space-x-4 h-full">
           <Link to="/signup">
-            <button className="border-[1px] border-gray-500 text-white font-bold bg-gray-500 px-2 py-1 rounded hover:bg-white hover:text-gray-700">
+            <button
+              className={`${
+                user ? "hidden" : ""
+              } border-[1px] border-gray-500 text-white font-bold bg-gray-500 px-2 py-1 rounded hover:bg-white hover:text-gray-700`}
+            >
               Sign-up
             </button>
           </Link>
 
           <Link onClick={handleAuth} to={!user && "/login"}>
             <button className="border-[1px] border-blue-400 text-blue-400 font-bold px-2 py-1 rounded hover:bg-blue-400 hover:text-white">
-              {userCopy ? "Logout" : "Login"}
+              {user ? "Logout" : "Login"}
             </button>
           </Link>
         </div>
 
         <div
           id="menu"
-          className="absolute space-x-5 text-white shadow-xl md:hidden right-0 top-[80%] bg-gray-500 p-3 rounded"
+          className={`absolute hidden ${
+            !user ? "space-x-5" : ""
+          } text-white shadow-xl md:hidden right-0 top-[80%] bg-gray-500 p-3 rounded`}
         >
-          <Link to={!user && "/login"}>
+          <Link onClick={handleAuth} to={!user && "/login"}>
             <button className="hover:underline underline-offset-2">
-              {userCopy ? "Logout" : "Login"}
+              {user ? "Logout" : "Login"}
             </button>
           </Link>
 
           <Link to={!user && "/signup"}>
-            <button className="hover:underline underline-offset-2">
+            <button
+              className={`hover:underline underline-offset-2 ${
+                user ? "hidden" : ""
+              }`}
+            >
               Sign-up
             </button>
           </Link>
